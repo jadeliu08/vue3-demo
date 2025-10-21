78dfad8f157b7cf5c033a5101a7434aa3366ff89
jadeliu08 <changqingliu08@163.com>
2025-10-21 20:56:01 +0800

    更新ts配置文件

--- src/tsDemo/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.users = exports.name = void 0;
-exports.handler = handler;
-exports.name = 'linbudu';
-function handler(input) { }
-exports.users = [];
--- src/tsDemo/index.ts
+++ src/tsDemo/index.ts
@@ -1,29 +1,106 @@
-export const name: string = 'linbudu';
-
+/*
 export function handler(input: string): void { }
+* */
 
+/*
 export interface IUser {
   name: string;
   age: number;
   sex?: number;
 }
 
-export const users: IUser[] = [];
-
 type A = Partial<IUser>;
-
-const a: A = { name: '' };
-
 type B = Required<IUser>;
+type C = Pick<IUser, 'name'>;
 
-const b: B = { age: 1, name: '', sex: 0 };
-b.age = 2;
-b.name = 'jade';
+export const users: IUser[] = [];
+*/
 
-type C = Pick<IUser, 'name' | 'age' | 'sex'>;
-
-
+/*
 type SayHello<T extends string | number> = `Hello ${T}`;
 
 const s1: SayHello<string> = 'Hello 12';
 const s2: SayHello<number> = 'Hello 34';
+*/
+
+/*interface Animal {
+  eat(): void;
+}
+
+interface Bnimal {
+  wang(): void;
+}
+
+interface Dog extends Animal {
+  bite(): void;
+}*/
+
+/*
+const APP = ['TaoBao', 'Tmall', 'Alipay'] as const;
+type app = typeof APP[number];
+ */
+
+
+/*
+ type Person = { age: number; name: string; alive: boolean };
+ type Age = Person['age];
+ */
+
+
+/*
+const MyArray = [
+  { name: "Alice", age: 15 },
+  { name: "Bob", age: 23 },
+  { name: "Eve", age: 38 },
+] as const;
+type Person = typeof MyArray[number];
+const p1: Person = {};
+*/
+
+/*
+const MyArray2 = [
+  { name: "Alice", age: 15 },
+  { name: "Bob", age: 23 },
+  { name: "Eve", age: 38 },
+];
+export type Person2 = typeof MyArray2;
+const p2: Person2 = {};
+*/
+
+/*const ss = Symbol('symbol');
+interface Person {
+  name: string;
+  age: number;
+  sex: boolean;
+  at: symbol;
+  1: number;
+  [ss]: symbol;
+}
+type PropEventSource<T> = {
+  on<K extends string & keyof T>(eventName: `${K}Changed`, callback: (newValue: T[K]) => void): void;
+}
+
+const pes: PropEventSource<Person> = {
+  on: () => {},
+};
+
+pes.on('atChanged', (value) => {
+
+});*/
+
+
+/*
+as const用法
+1、函数内部
+type Colour = 'red' | 'green' | 'blue';
+type Variant = 'light' | 'dark';
+function createColourVariant(colour: Colour, variant: Variant) {
+  return `${variant}-${colour}` as const;
+}
+const dd = createColourVariant('red', 'dark');
+* 2、对象用法
+* const obj = { name: 'ttt', foo: { count: 1 }} as const;
+* 3、数组用法
+  const my = ['Tim', 'Tony', 'Bob'] as const;
+  const my = [{ name: ''}, { name: '' }] as const;
+*/
